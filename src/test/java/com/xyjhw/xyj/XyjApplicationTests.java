4b6936588349ac15802e54dbd3e75e9c4693373e
ibaced <1779323500@qq.com>
2019-04-30 15:46:25 +0800

    test类之前出了问题，无法部署，现在修改完了

--- src/test/java/com/xyjhw/xyj/XyjApplicationTests.java
+++ src/test/java/com/xyjhw/xyj/XyjApplicationTests.java
@@ -1,29 +1,17 @@
 package com.xyjhw.xyj;
-
-import java.util.List;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
-import com.xyjhw.xyj.XyjApplication;
-import com.xyjhw.xyj.dao.ActionDAO;
-import com.xyjhw.xyj.pojo.Action;
-import com.xyjhw.xyj.mapper.ActionMapper;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(classes = XyjApplication.class)
 public class XyjApplicationTests {
 
-    @Autowired ActionDAO dao;
+
 
     @Test
     public void test() {
-        List<Action> cs=  dao.findAll();
-        for (Action c : cs) {
-            System.out.println("c.getName():"+ c.getName());
-        }
-
     }
 }
